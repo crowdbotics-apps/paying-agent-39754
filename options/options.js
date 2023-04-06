7d68f042c989d687c45faf42d54fbf4bf3c3c13a
Crowdbotics <team@crowdbotics.com>
2023-04-06 18:15:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "paying-agent-39754",
+  url: "https://paying-agent-39754.botics.co",
+  api: "https://paying-agent-39754.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
