a913f3ec1a5358e17dbc00811ba3a47de8914326
Noah Cho <noah.cho@crowdbotics.com>
2023-04-07 17:17:05 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "paying-agent-39754",
-  url: "https://paying-agent-39754.botics.co",
-  api: "https://paying-agent-39754.botics.co/api/v1"
-}
+  "name": "paying-agent-39754",
+  "url": "https://paying-agent-39754.botics.co",
+  "api": "https://paying-agent-39754.botics.co/api/v1",
+  "initialRoute": "BlankScreen18308"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
